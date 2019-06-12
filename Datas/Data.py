03a9fe8025b00d7b3f30037179a7318f9662c38d
DurkanKaya <47315745+DurkanKaya@users.noreply.github.com>
2019-06-12 17:36:06 +0300

    değişiklik

--- Datas/Data.py
+++ Datas/Data.py
@@ -25,10 +25,6 @@ conn_str = (
 
 cnxn = connect(conn_str)
 cursor = cnxn.cursor()
-training_images = []
-training_labels = []
-test_images = []
-test_labels = []
 
 
 def get_sinif_list():
@@ -46,7 +42,7 @@ def get_sinif_list():
     return array
 
 
-def onehotlabel(sinif, siniflist):
+def one_hot_label(sinif, siniflist):
     label = np.zeros((siniflist.__len__(),), dtype=int)
     finding_index = siniflist.index(sinif)
     label[finding_index] = 1
@@ -57,12 +53,14 @@ def onehotlabel(sinif, siniflist):
 
 def read_train_images(heigh, width):
     siniflist = get_sinif_list()
+    training_images = []
+    training_labels = []
     for sinif in siniflist:
         path = os.path.join(os.getcwd(), "images",  sinif.foldername)
         for filename in os.listdir(path):
             file_content = pilimage.open(path + "\\" + filename)
             im = file_content.resize((width, heigh), pilimage.ANTIALIAS)
-            training_labels.append(onehotlabel(sinif, siniflist))
+            training_labels.append(one_hot_label(sinif, siniflist))
             training_images.append([np.array(im)])
             pass
         row = cursor.fetchone()
@@ -78,6 +76,8 @@ def read_train_images(heigh, width):
 
 def read_test_images(heigh, width):
     siniflist = get_sinif_list()
+    test_images = []
+    test_labels = []
     for sinif in siniflist:
         cursor.execute('select id, foldername, filename from tbl_01_01_testimage a where id = ?', sinif.id)
         row = cursor.fetchone()
@@ -86,7 +86,7 @@ def read_test_images(heigh, width):
             if os.path.exists(path):
                 file_content = pilimage.open(path)
                 im = file_content.resize((width, heigh), pilimage.ANTIALIAS)
-                test_labels.append(onehotlabel(sinif, siniflist))
+                test_labels.append(one_hot_label(sinif, siniflist))
                 test_images.append([np.array(im)])
             row = cursor.fetchone()
         pass
@@ -258,23 +258,19 @@ def delete_supererogator_rect(candidates):
         candidates.remove(i)
 
 
-def random_batch(batch_size, images_size, is_training=True, append_preprocess=False):
-    images = []
-    labels = []
+def random_batch(batch_size, images, labels, append_preprocess=False):
+    batch_images = []
+    batch_labels = []
     for i in range(batch_size):
-        random_number = np.random.randint(0, images_size)
-        if is_training:
-            labels.append(training_labels[random_number])
-            image = training_images[random_number]
-        else:
-            labels.append(test_labels[random_number])
-            image = test_images[random_number]
+        random_number = np.random.randint(0, images.__len__())
+        batch_labels.append(labels[random_number])
+        img = images[random_number]
 
         if append_preprocess:
-            pre_process_image(image)
+            pre_process_image(img)
 
-        images.append(image)
-    return images, labels
+        batch_images.append(img)
+    return batch_images, batch_labels
 
 
 class DataSinif:
